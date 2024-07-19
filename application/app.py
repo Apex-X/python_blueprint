9abd2d7e6a72d91ad1ac4ed4a2a1d4b5adcee816
n25a <nimashadix@gmail.com>
2024-07-20 00:53:27 +0330

    🫃 add application class

---
+++ application/app.py
@@ -0,0 +1,26 @@
+from typing import Optional
+from .database import Database
+from config import cfg
+import redis
+
+
+class Application:
+    db: Database = None
+    redis_client = None
+
+    def __init__(self):
+        self.db = Database()
+        self.redis_client = redis.Redis(
+            host=cfg.redis.host,
+            port=cfg.redis.port,
+            password=cfg.redis.password,
+            db=cfg.redis.db,
+        )
+
+
+app: Optional[Application] = None
+
+
+def load_app():
+    global app
+    app = Application()
